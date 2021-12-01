f44d56735cd761992e54bfb1130d04091e69162e
xFIREx2005 <78804981+xFIREx2005@users.noreply.github.com>
2021-12-01 08:07:52 -0300

    Update DestroyAnimations.cs

--- Assets/Player/Codes/DestroyAnimations.cs
+++ Assets/Player/Codes/DestroyAnimations.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroySmokeRun : MonoBehaviour
+public class DestroyAnimations : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
